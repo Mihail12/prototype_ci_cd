5be61e144b6bb90ce758a9f0ae77a377b0f09f68
mihail12 <spasenkomihail@gmail.com>
2020-10-27 17:17:52 +0200

    travis

--- f_app.py
+++ f_app.py
@@ -144,6 +144,7 @@ def on_room(*args, **kwargs):
 def on_room(*args, **kwargs):
     print('connected schedule_task')
 
+
 if __name__ == "__main__":
 
     socketio.run(flask_app, debug=True, host="0.0.0.0")
