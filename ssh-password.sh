ecc17dcd8d9dfff34773d7ca5e29aee4bf459384
mihail12 <spasenkomihail@gmail.com>
2020-10-28 00:12:29 +0200

    travis

--- ssh-password.sh
+++ ssh-password.sh
@@ -1,6 +1,15 @@
-#!/usr/bin/expect -f
-spawn ssh-add /tmp/deploy_key
-expect "Enter passphrase for /tmp/deploy_key:"
-send "travis\n";
-expect "Identity added: /tmp/deploy_key (/tmp/deploy_key)"
-interact
+#!/bin/bash
+
+if [ $# -ne 1 ] ; then
+  echo "Usage: ssh-add-pass keyfile passfile"
+  exit 1
+fi
+
+eval $(ssh-agent)
+
+expect << EOF
+  spawn ssh-add $1
+  expect "Enter passphrase"
+  send "\n"
+  expect eof
+EOF
