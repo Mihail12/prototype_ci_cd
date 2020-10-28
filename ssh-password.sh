2958b3cc4f15781d21f7c85714f0bd18cc00daf9
mihail12 <spasenkomihail@gmail.com>
2020-10-28 08:49:28 +0200

    travis

--- ssh-password.sh
+++ ssh-password.sh
@@ -4,7 +4,7 @@ if [ $# -ne 1 ] ; then
   echo "Usage: ssh-add-pass keyfile passfile"
   exit 1
 fi
-
+chmod 400 /tmp/deploy_key
 expect << EOF
   spawn ssh -o LogLevel=ERROR -i /tmp/deploy_key ubuntu@35.158.109.210 'bash ~/prototype_ci_cd/deploy.sh'
   expect "Enter passphrase"
