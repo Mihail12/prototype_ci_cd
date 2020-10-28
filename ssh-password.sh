65761f767bf4992201333b5e1b9f65dcd521e9d4
mihail12 <spasenkomihail@gmail.com>
2020-10-28 12:25:06 +0200

    travis

--- ssh-password.sh
+++ ssh-password.sh
@@ -6,12 +6,18 @@ if [ $# -ne 1 ] ; then
 fi
 
 expect << EOF
+  spawn ssh-keygen -y -f deploy_key
+  expect "Enter passphrase"
+  send "travis\r"
+  expect eof
+EOF
+
+expect << EOF
   spawn ssh-add deploy_key
   expect "Enter passphrase"
   send "travis\r"
   expect eof
 EOF
-ssh-keyscan 35.158.109.210 >> ~/.ssh/known_hosts
 
 expect << EOF
   spawn ssh -o LogLevel=ERROR -i deploy_key ubuntu@35.158.109.210 'bash ~/prototype_ci_cd/deploy.sh'
