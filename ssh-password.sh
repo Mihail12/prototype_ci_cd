1a7d4b756fc3f1b32d92c3d88f4451fcdf242264
mihail12 <spasenkomihail@gmail.com>
2020-10-28 09:46:28 +0200

    travis

--- ssh-password.sh
+++ ssh-password.sh
@@ -13,7 +13,7 @@ expect << EOF
 EOF
 
 expect << EOF
-  spawn ssh -o LogLevel=ERROR -i deploy_key ubuntu@35.158.109.210 'bash ~/prototype_ci_cd/deploy.sh'
+  spawn ssh -o LogLevel=ERROR ubuntu@35.158.109.210 'bash ~/prototype_ci_cd/deploy.sh'
   expect "Enter passphrase"
   send "travis\r"
   expect eof
